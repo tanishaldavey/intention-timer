cbeeaf0cac0cc99564e770788fd289d09a1818f7
Tanisha L Davey <tanisha.davey2@gmail.com>
2019-10-14 12:34:25 -0600

    Customize input field to not accept the letter e

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
 var minutes = document.querySelector('.minutes');
-console.log(minutes);
 var seconds = document.querySelector('.seconds');
-console.log(seconds);
+var startButton = document.querySelector('.start-btn');
+
+
+minutes.addEventListener('keyup', checkInputMinutes);
+seconds.addEventListener('keyup', checkInputSeconds);
+
+
+//Checks if the minutes input field contains the letter e.
+function checkInputMinutes() {
+  if (event.keyCode === 69) {
+    alert('Minutes must contain a number between 0 and 60.');
+  }
+}
+//Checks if the seconds input field contains the letter e.
+function checkInputSeconds() {
+  if (event.keyCode === 69) {
+    alert('Seconds must contain a number between 0 and 60.');
+  }
+}
