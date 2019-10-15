9ebb09829abccc616f26ad08e0ca7b8263c6442e
Tanisha L Davey <tanisha.davey2@gmail.com>
2019-10-15 15:39:44 -0600

    Edit comments

--- main.js
+++ main.js
@@ -3,9 +3,10 @@ var seconds = document.querySelector('.seconds');
 var startButton = document.querySelector('.start-btn');
 var warning = document.querySelector('.warning');
 var question = document.querySelector('.question');
-var warningQuestion = document.querySelector('.warning-question')
-var warningMinutes = document.querySelector('.warning-minutes')
-var warningSeconds = document.querySelector('.warning-seconds')
+var warningQuestion = document.querySelector('.warning-question');
+var warningMinutes = document.querySelector('.warning-minutes');
+var warningSeconds = document.querySelector('.warning-seconds');
+var timerInterface = document.querySelector('.timer-interface');
 
 minutes.addEventListener('keyup', checkInputMinutes);
 seconds.addEventListener('keyup', checkInputSeconds);
@@ -24,7 +25,7 @@ function checkInputSeconds() {
     alert('Seconds must contain a number between 0 and 60.');
   }
 }
-//Checks if inputs are empty before proceeding to next screen.
+//Checks if inputs are empty before proceeding to the timer screen.
 function checkInput(event) {
   if (question.value === '') {
     warningQuestion.classList.toggle('warning-question');
