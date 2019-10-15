257844d346042cb26773f2971a07249b0fb24998
Tanisha L Davey <tanisha.davey2@gmail.com>
2019-10-15 16:30:07 -0600

    Move functions under corresponding variables

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 var minutes = document.querySelector('.minutes');
 var seconds = document.querySelector('.seconds');
 var startButton = document.querySelector('.start-btn');
+
+//Checks if the minutes input field contains the letter e.
+function checkInputMinutes() {
+  if (event.keyCode === 69) {
+    alert('Minutes must contain a number between 0 and 60.');
+  }
+}
+//Checks if the seconds input field contains the letter e.
+function checkInputSeconds() {
+  if (event.keyCode === 69) {
+    alert('Seconds must contain a number between 0 and 60.');
+  }
+}
 // VARIABLES BEGIN
 var study = document.getElementById('study');
 var meditate = document.getElementById('meditate');
@@ -21,19 +34,19 @@ var h5Exercise = document.getElementById('h5-exercise');
 changeStudyBtn.addEventListener('click', function () {
   study.style.border = '2px solid #B3FD78';
   h5Study.style.color = '#B3FD78';
-  studyImg.src = '../images/study-active.svg';
+  studyImg.src = 'images/study-active.svg';
 });
 
 changeMeditateBtn.addEventListener('click', function () {
   meditate.style.border = '2px solid #C278FD';
   h5Meditate.style.color = '#C278FD';
-  meditateImg.src = '../images/meditate-active.svg';
+  meditateImg.src = 'images/meditate-active.svg';
 });
 
 changeExerciseBtn.addEventListener('click', function () {
   exercise.style.border = '2px solid #FD8078';
   h5Exercise.style.color = '#FD8078';
-  exerciseImg.src = '../images/exercise-active.svg';
+  exerciseImg.src = 'images/exercise-active.svg';
 });
 // Buttons
 minutes.addEventListener('keyup', checkInputMinutes);
@@ -41,18 +54,7 @@ seconds.addEventListener('keyup', checkInputSeconds);
 startButton.addEventListener('click', function);
 
 
-//Checks if the minutes input field contains the letter e.
-function checkInputMinutes() {
-  if (event.keyCode === 69) {
-    alert('Minutes must contain a number between 0 and 60.');
-  }
-}
-//Checks if the seconds input field contains the letter e.
-function checkInputSeconds() {
-  if (event.keyCode === 69) {
-    alert('Seconds must contain a number between 0 and 60.');
-  }
-}
+
 
 //create a classList.add
 //classList.add.toggle() for minutes and seconds
